4a9ac429c7c17483208b16988f13b488dc4cc4f1
viniciustmoraes <tmmvinicius@gmail.com>
2024-03-25 21:04:57 +0100

    Code for special_days curve calculation

--- special_days.py
+++ special_days.py
@@ -1,45 +1,100 @@
 import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
+import copy
+from datetime import datetime, time
+import warnings
 
-# Chooses the special day to be analyzed
-special_day = "Jogos da Copa"
-
-special_index = 4
-date_index = 2
-
-# Maps the days when these special days occur
-dates = set()
-with open("databases/dados_diaespecial.csv", "r") as file:
-    csv_reader = csv.reader(file)
-    headers = next(csv_reader)
-
-    for row in csv_reader:
-        row = row[0].split(";")
-        if row[special_index] == special_day:
-            dates.add(row[date_index])
-
-# Opens the charge dataset
-
-with open("databases/dados_carga.csv", "r") as file:
-    csv_reader = csv.reader(file)
-    headers = next(csv_reader)
+warnings.filterwarnings("ignore")  # Remove
 
 
-# Finds the index of a date in a given dates array
-def binary_search(dates: np.ndarray, target_date: str):
-    left = 0
-    right = len(dates) - 1
+def main(special_day: str):
+    # Reads the database
+    file = "databases/dados_carga_diasespeciais.xlsx"
 
-    while left <= right:
-        mid = (left + right) // 2
-        if dates[mid] == target_date:
-            return mid  # Found the target date
-        elif dates[mid] < target_date:
-            left = mid + 1
+    print("Reading file...")
+    df = pd.read_excel(file)
+    print(f"Ok. {len(df)} data points have been read.")
+
+    ### df Treatment ###
+
+    # Only keeps the south east region
+    df = df[df["cod_areacarga"] == "SECO"]
+    df.drop(columns=["cod_areacarga"], inplace=True)
+
+    # Removes extra spaces, and converts dates to datetime
+    df["Data especial"] = df["Data especial"].str.strip()
+    df["din_referenciautc"] = pd.to_datetime(df["din_referenciautc"])
+
+    # Converts numbers to proper format
+    df.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8]] = df.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8]].astype(
+        str
+    )
+
+    df.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8]] = (
+        df.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8]]
+        .applymap(lambda x: x.replace(",", "."))
+        .astype(float)
+    )
+
+    df_special = df[df["Data especial"] == special_day]
+
+    # Group the DataFrame by day
+    grouped = df_special.groupby(df_special["din_referenciautc"].dt.date)
+
+    # Create a dictionary to store the DataFrames
+    dfs_by_day = []
+
+    # Iterate over the groups and store each DataFrame in the list
+    for day, group_df in grouped:
+        dfs_by_day.append(group_df)
+
+    adjusted_dfs_by_day = []
+    tol = 45
+
+    for df_day in dfs_by_day:
+        if df_day.shape[0] == 48:
+            df_day.index = range(1, len(df_day) + 1)
+            adjusted_dfs_by_day.append(df_day)
+        elif df_day.shape[0] >= tol:
+            # Repeat the first row of the DataFrame twice
+            new_values = pd.concat([df_day.iloc[[0]]] * (48 - df_day.shape[0]))
+            # Concatenate the new values with the original DataFrame
+            df_day = pd.concat([new_values, df_day])
+            df_day.index = range(1, len(df_day) + 1)
+            adjusted_dfs_by_day.append(df_day)
+
+    # Calculates the mean values
+    df_concat = pd.concat(adjusted_dfs_by_day)
+
+    by_row_index = df_concat.groupby(df_concat.index)
+    df_means = by_row_index.mean()
+
+    # Corrects time shift
+    delta_t = -3
+    carga_global_media = np.roll(np.array(df_means["val_cargaglobal"]), delta_t)
+
+    fig, ax = plt.subplots()
+    x_ax = [x / 2 for x in range(48)]
+    ax.plot(x_ax, carga_global_media)
+    plt.show()
+    return carga_global_media
+
+
+def same_day(x: datetime, y: datetime) -> bool:
+    """
+    Returns True if x and y are in the same day, and False if not
+    """
+    try:
+        if x.day == y.day and x.month == y.month and x.year == y.year:
+            return True
         else:
-            right = mid - 1
+            return False
+    except:
+        return False
 
-    # If the target date is not found in the list, return -1
-    return -1
+
+if __name__ == "__main__":
+    special_day = "Dia de Sao Sebastiao"
+    print(main(special_day=special_day))
